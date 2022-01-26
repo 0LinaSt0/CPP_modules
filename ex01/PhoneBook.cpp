22205620ec6215ce6404b4d3715147023db96d92
Полина Христофорова <polina@MacBook-Air-Polina.local>
2022-01-27 01:06:41 +0300

    complitings:00 & in_work:01

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -11,22 +11,66 @@
 /* ************************************************************************** */
 
 #include "PhoneBook.hpp"
+void	cout_contact(PhoneBook& tele_libro, int i){
+	Contact cont;
+	
+	tele_libro.take_contact(i, cont);
+	cont.print_index();
+	cont.print_first_name();
+	cont.print_last_name();
+	cont.print_nickname();
+}
+void	search_contact(PhoneBook& tele_libro){
+	int	i_contacts = 0;
+
+	std::cout << "index     " << " | " << "first name"
+			<< " | " << "last name " << " | " << "nickname  " << std::endl;
+	std::cout << "-------------------------------------------------" << std::endl;
+	if(tele_libro.eight_fl == 8){
+		while(i_contacts < tele_libro.eight_fl){
+			cout_contact(tele_libro, i_contacts++);
+		}
+	}else{
+		while(i_contacts < tele_libro.index_count){
+			cout_contact(tele_libro, i_contacts++);
+		}
+	}
+	std::cout << "-------------------------------------------------" << std::endl;
+	//function about shoing special contact
+}
 
 void	add_contact(PhoneBook& tele_libro){
 	Contact	new_cont;
+	std::string	input_str;
 
+	tele_libro.eight_fl = 0;
 	std::cout << "-->Please, input some information about new contact"
 			<< std::endl;
-	std::cout << "	First name:";
-	std::cin >> new_cont.first_name;
-	std::cout << "	Last name:";
-	std::cin >> new_cont.last_name;
-	std::cout << "	Nickname:";
-	std::cin >> new_cont.nickmame;
-	std::cout << "	Phone number:";
-	std::cin >> new_cont.phone_number;
-	std::cout << "	Darkest secret:";
-	std::cin >> new_cont.darkest_secret;
+	std::cout << "	First name: ";
+	std::cin >> input_str;
+	new_cont.take_first_name(input_str);
+	std::cout << "	Last name: ";
+	std::cin >> input_str;
+	new_cont.take_last_name(input_str);
+	std::cout << "	Nickname: ";
+	std::cin >> input_str;
+	new_cont.take_nickname(input_str);
+	std::cout << "	Phone number: ";
+	std::cin >> input_str;
+	new_cont.take_phone_number(input_str);
+	std::cout << "	Darkest secret: ";
+	std::cin >> input_str;
+	new_cont.take_darkest_secret(input_str);
+	new_cont.take_index();
+	tele_libro.take_count_index();
+	if (tele_libro.index_count == 0){
+		tele_libro.eight_fl = 8;
+	}
+	if (tele_libro.index_count > 8){
+		tele_libro.index_count = 1;
+	}
+		tele_libro.add_new_contact(new_cont, (tele_libro.index_count - 1));
+	std::cout << "!New contact was added!" << std::endl << std::endl;
 }
 
 int	main(int argc, char **argv){
@@ -42,18 +86,18 @@ int	main(int argc, char **argv){
 	// 	std::cout << "Try without any arguments" << std::endl;
 	// 	return (1);
 	// }
-
 	while (1){
 		std::cin >> out_str;
-		if (out_str.compare("ADD") == 0)
-			std::cout << "ADD" << std::endl;
+		if (out_str.compare("ADD") == 0){
+			add_contact(tele_libro);
+		}
 		else if (out_str.compare("SEARCH") == 0)
 			std::cout << "SEARCH" << std::endl;
 		else if (out_str.compare("EXIT") == 0)
 			return (0);
 		else{
 			std::cout << "-->Programm takes one of three arguments: ADD, SEARCH or EXIT:"
-				<< std::endl << "pleas try one of that" << std::endl;
+				<< std::endl << "	pleas try one of that" << std::endl;
 		}
 	}
 	return (0);
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -18,17 +18,59 @@
 class Contact{
 private:
 	size_t		index;
-public:
 	std::string	first_name;
 	std::string	last_name;
-	std::string	nickmame;
+	std::string	nickname;
 	std::string	phone_number;
 	std::string	darkest_secret;
-	void	take_index(size_t num_elem){
-		this->index = num_elem;
+public:
+	void	take_index(){
+		index++;
 	}
 	void	print_index(){
-		std::cout << this->index << std::endl;
+		std::cout << "  " << index << "       " << " | ";
+	}
+	void	take_first_name(std::string str){
+		first_name = str;
+	}
+	void	print_first_name(){
+		if (first_name.length() == 10){
+			std::cout << "  " << first_name << "       " << " | ";
+		}else if(first_name.length() > 10){
+			//printing just 9 characters wirh dot
+		}else{
+			//printig all string and puting spaces for 10 chars
+		}
+	}
+	void	take_last_name(std::string str){
+		last_name = str;
+	}
+	void	print_last_name(){
+		if (last_name.length() == 10){
+			std::cout << "  " << last_name << "       " << " | ";
+		}else if(last_name.length() > 10){
+			//printing just 9 characters wirh dot
+		}else{
+			//printig all string and puting spaces for 10 chars
+		}
+	}
+	void	take_nickname(std::string str){
+		nickname = str;
+	}
+	void	print_nickname(){
+		if (nickname.length() == 10){
+			std::cout << "  " << first_name << "       " << " | ";
+		}else if(nickname.length() > 10){
+			//printing just 9 characters wirh dot
+		}else{
+			//printig all string and puting spaces for 10 chars
+		}
+	}
+	void	take_phone_number(std::string str){
+		phone_number = str;
+	}
+	void	take_darkest_secret(std::string str){
+		darkest_secret = str;
 	}
 };
 
@@ -36,10 +78,20 @@ class PhoneBook{
 private:
 	Contact	book_arr[8];
 public:
-	size_t	index;
-	
+	size_t	index_count;
+	int		eight_fl;
+
 	void	add_new_contact(Contact new_, size_t index_place){
 		book_arr[index_place] = new_;
+	}
+	void	take_contact(size_t index_place, Contact& cont){
+		cont = book_arr[index_place];
+	}
+	void	take_count_index(){
+		index_count++;
+	}
+	void	print_index(){
+		std::cout << index_count << std::endl;
 	}
 };
 
